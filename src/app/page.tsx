7a747b077d09516eac2d947ac6267b64ed77f707
muminin <almin.cesc@gmail.com>
2024-03-25 13:48:07 +0700

    success deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Hero } from "@/components/hero";
 import PostSection from "@/components/postSection";
 import ProfileSection from "@/components/profileSection";
 import YoutubeSection from "@/components/youtubeSection";
+import { deletePostBySlug, getAllSlug } from "@/services/postService";
 
 import { FC } from "react";
 
@@ -12,6 +13,10 @@ interface pageProps {
 }
 
 const page: FC<pageProps> = async ({ params: {} }) => {
+  // console.log(
+  //   await deletePostBySlug("stronghari-tenang-untuk-persiapan-ujianstrong"),
+  // );
+  // console.log(await getAllSlug(), "in main page.tsx");
   return (
     <>
       <Header />
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -3,12 +3,13 @@ import SinglePost from "@/components/singlePost";
 import type { NextPage } from "next";
 import { getAllSlug, getPostBySlug } from "@/services/postService";
 import { Header } from "@/components/header";
+import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
   const allSlug = await getAllSlug();
 
   return allSlug.map((item) => {
-    return { slug: item };
+    return { slug: item ?? "404" };
   });
 }
 
@@ -17,6 +18,7 @@ interface Props {
 }
 
 const Post: NextPage<Props> = async ({ params: { slug } }) => {
+  if (slug == "404") return notFound();
   const post = await getPostBySlug(slug);
 
   return (
